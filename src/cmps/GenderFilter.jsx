5c698d5bcf2cc1116f4b61b08380fb5c302de71c
Tom Ofek Rytt <tomofekrytt@gmail.com>
2022-02-08 17:11:11 +0200

    fixed layout and removed some console logs

--- src/cmps/GenderFilter.jsx
+++ src/cmps/GenderFilter.jsx
@@ -1,50 +1,49 @@
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-import { connect, useDispatch } from "react-redux";
+import {connect, useDispatch} from 'react-redux';
 
-import {sortByGender} from "../store/item.action.js"
+import {sortByGender} from '../store/item.action.js';
 
 // const options = [
 //     'HIM', 'HER', 'BOTH'
 // ];
 
 const options = [
-    { value: 3, label: 'HIM' },
-    { value: 4, label: 'HER' },
-    { value: 1, label: 'BOTH' }
-]
-const defaultOption = "Gender";
+	{value: 3, label: 'HIM'},
+	{value: 4, label: 'HER'},
+	{value: 1, label: 'BOTH'},
+];
+const defaultOption = 'Gender';
 export function _GenderFilter({priceRange}) {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-    const handleChange = (event) => {
-        const gender = event
-        console.log(gender)
-        dispatch(sortByGender(gender, priceRange))
-    };
+	const handleChange = (event) => {
+		const gender = event;
+		dispatch(sortByGender(gender, priceRange));
+	};
 
-    return (
-        <div>
-            <Dropdown
-                className="gender-filter"
-                options={options}
-                onChange={handleChange}
-                value={defaultOption}
-                placeholder="GENDER" />
-
-        </div >
-    )
+	return (
+		<div>
+			<Dropdown
+				className='gender-filter'
+				options={options}
+				onChange={handleChange}
+				value={defaultOption}
+				placeholder='GENDER'
+			/>
+		</div>
+	);
 }
 
-function mapStateToProps({ itemModule }) {
-    return {
-        gender: itemModule.gender,
-        priceRange: itemModule.priceRange,
-    };
+function mapStateToProps({itemModule}) {
+	return {
+		gender: itemModule.gender,
+		priceRange: itemModule.priceRange,
+	};
 }
 const mapDispatchToProps = {
-    sortByGender,
+	sortByGender,
 };
 
 export const GenderFilter = connect(mapStateToProps, mapDispatchToProps)(_GenderFilter);
