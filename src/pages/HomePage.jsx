cf9cee53035235668dd2588ce975d071e33579ed
Tom Ofek Rytt <tomofekrytt@gmail.com>
2022-02-08 14:36:47 +0200

    cleaned file cmps for project

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,50 +1,44 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
-import { CrudlService } from '../services/crudl.service.js';
-import {
-  loadItems,
-  addItems,
-  updateItems,
-  removeItems,
-} from '../store/item.action.js';
+import {CrudlService} from '../services/crudl.service.js';
+import {loadItems, addItems, updateItems, removeItems} from '../store/item.action.js';
 
 export function HomePage() {
-  const dispatch = useDispatch();
-  const { stores } = useSelector((state) => ({ stores: state.itemModule.stores }));
+	const dispatch = useDispatch();
+	const {stores} = useSelector((state) => ({stores: state.itemModule.stores}));
 
+	useEffect(() => {
+		getItems();
+	}, []);
 
-  useEffect(async () => {
-    await getItems();
-
-  }, []);
-
-  const getItems = async () => {
-    // console.log('you are in load product in home page ');
-    dispatch(loadItems());
-  };
-  console.log(stores)
-  if (!stores.length) return <div>Loading...</div>;
-  return (
-    <section className='home-page'>
-      <h1 className="hero-title">HomePage</h1>
-      <section className='grid-list flex-center'>
-        {stores && stores.map((store) => {
-          return store.Products.map(product => {
-            return (<div className='product-preview' key={product.ProductId}>
-              <div className="product-img-container">
-                <img className="product-img" src={product.ProductImage} alt=''/>
-              </div>
-              <div className="product-details">
-                <h2 className='store-name'> {store.StoreName}</h2>
-                <h3 className='product-price'> {product.ProductTitle}</h3>
-              </div>
-            </div>
-            )
-
-          })
-        })}
-      </section>
-    </section>
-  );
+	const getItems = async () => {
+		// console.log('you are in load product in home page ');
+		dispatch(loadItems());
+	};
+	console.log(stores);
+	if (!stores.length) return <div>Loading...</div>;
+	return (
+		<section className='home-page'>
+			<h1 className='hero-title'>HomePage</h1>
+			<section className='grid-list flex-center'>
+				{stores &&
+					stores.map((store) => {
+						return store.Products.map((product) => {
+							return (
+								<div className='product-preview' key={product.ProductId}>
+									<div className='product-img-container'>
+										<img className='product-img' src={product.ProductImage} alt='' />
+									</div>
+									<div className='product-details'>
+										<h2 className='store-name'> {store.StoreName}</h2>
+										<h3 className='product-price'> {product.ProductTitle}</h3>
+									</div>
+								</div>
+							);
+						});
+					})}
+			</section>
+		</section>
+	);
 }
--- src/pages/SecondPage.jsx
+++
@@ -1,7 +0,0 @@
-export function SecondPage() {
-  return (
-    <section className='second-page'>
-      <h1>Second Page</h1>
-    </section>
-  );
-}
