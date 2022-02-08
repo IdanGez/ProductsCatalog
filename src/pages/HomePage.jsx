f6a4440ebae5c167ddf3589641a9b4ca3d384945
Tom Ofek Rytt <tomofekrytt@gmail.com>
2022-02-08 17:11:31 +0200

    fixed layout and removed some console logs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,33 +3,32 @@ import {useDispatch, useSelector} from 'react-redux';
 
 import {CrudlService} from '../services/crudl.service.js';
 import {GenderFilter} from '../cmps/GenderFilter.jsx';
-import {PriceRangeFilter} from '../cmps/PriceRangeFilter.jsx';
+import {PriceRangeFilter} from '../cmps/PriceRangeFilter';
 import {loadItems, addItems, updateItems, removeItems} from '../store/item.action.js';
 
 export function HomePage() {
 	const dispatch = useDispatch();
 	const {stores} = useSelector((state) => ({stores: state.itemModule.stores}));
 
-	// useEffect(() => {
-	// 	getItems();
-	// }, []);
+	useEffect(() => {
+		getItems();
+	}, []);
 
 	useEffect(async () => {
 		await getItems();
 	}, []);
 
 	const getItems = async () => {
-		// console.log('you are in load product in home page ');
 		dispatch(loadItems());
 	};
-	// console.log(stores);
 	if (!stores.length) return <div>Loading...</div>;
 	return (
 		<section className='home-page'>
-			<h1 className='hero-title'>HomePage</h1>
-			<GenderFilter />
-			<PriceRangeFilter />
-			<main className='grid-list flex-center'>
+			<section className='filters'>
+				<GenderFilter />
+				<PriceRangeFilter />
+			</section>
+			<main className='app-main grid-list flex-center'>
 				{stores &&
 					stores.map((store) => {
 						return store.Products.map((product) => {
@@ -39,14 +38,14 @@ export function HomePage() {
 										<img className='product-img' src={product.ProductImage} alt='' />
 									</div>
 									<div className='product-details'>
-										<h4 className='store-name'> {store.StoreName}</h4>
-										<h4 className='product-price'> {product.PriceLabel}</h4>
-										<h4 className='product-name'>
+										<h3 className='store-name'> {store.StoreName}</h3>
+										<h3 className='product-price'> {product.PriceLabel}</h3>
+										<h3 className='product-name'>
 											{' '}
-											{product.ProductTitle.length < 30
+											{product.ProductTitle.length < 20
 												? product.ProductTitle
-												: product.ProductTitle.slice(0, 25) + '...'}
-										</h4>
+												: product.ProductTitle.slice(0, 20) + '...'}
+										</h3>
 									</div>
 								</div>
 							);
--- src/services/crudl.service.js
+++ src/services/crudl.service.js
@@ -1,46 +1,40 @@
 import axios from 'axios';
-import { storageService } from './async-storage.service.js';
-import { loadFromStorage, saveToStorage } from './storage.service.js';
+import {storageService} from './async-storage.service.js';
+import {loadFromStorage, saveToStorage} from './storage.service.js';
 // import { utilService } from './util.service';
-import { httpService } from './http.service.js';
+import {httpService} from './http.service.js';
 
-import Data from '../data/products.json'
+import Data from '../data/products.json';
 
 const STORAGE_KEY_BOARD = 'board';
 
 export const CrudlService = {
-  query,
-  getById,
-  save,
-  remove,
-  update,
-  sortProducts,
- 
+	query,
+	getById,
+	save,
+	remove,
+	update,
+	sortProducts,
 };
 
 // If you want to use full crudl with storage use async storage service
 // If you want to use only load from storage and save to storage use storage service
 
 async function query() {
-  // console.log(Products);
-  // let boards = await storageService.query('board');
-  // const boards = await httpService.get(`board`);
-  // console.log(boards);
-  // return boards;
-  return Data;
+	return Data;
 }
 
 async function getStores() {
-  let data = await query()
-  let products = []
-  let { Stores } = data
-  let stores = Stores
-  // Stores.map(store => {
-  //   return store.Products.map(product => {
-  //     return products.push(product)
-  //   })
-  // })
-  return stores
+	let data = await query();
+	let products = [];
+	let {Stores} = data;
+	let stores = Stores;
+	// Stores.map(store => {
+	//   return store.Products.map(product => {
+	//     return products.push(product)
+	//   })
+	// })
+	return stores;
 }
 // async function getProducts() {
 //   let data = await query()
@@ -54,45 +48,45 @@ async function getStores() {
 //   return products
 // }
 async function sortProducts(gender = null, priceRange = null) {
-  let stores = await getStores()
-  // if (!gender && !priceRange) return products
-  let tags = []
-  tags.push(gender.value)
-  const filteredProdcuts = []
-  // stores.Products.filter
-  stores.forEach(store => {
-    store.filteredProdcuts = [];
-    store.Products.filter(product => {
-      let currProduct = tags.every(tag => {
-        return product.ProductTags.includes(tag)
-      })
-      if (currProduct) store.filteredProdcuts.push(product)
-      return store
-    })
-  })
-  console.log(stores)
-  return stores
+	let stores = await getStores();
+	// if (!gender && !priceRange) return products
+	let tags = [];
+	tags.push(gender.value);
+	const filteredProdcuts = [];
+	// stores.Products.filter
+	stores.forEach((store) => {
+		store.filteredProdcuts = [];
+		store.Products.filter((product) => {
+			let currProduct = tags.every((tag) => {
+				return product.ProductTags.includes(tag);
+			});
+			if (currProduct) store.filteredProdcuts.push(product);
+			return store;
+		});
+	});
+	console.log(stores);
+	return stores;
 }
 
 async function getById(boardId) {
-  // console.log(boardId);
-  // const board = await storageService.get('board', boardId);
-  const board = await httpService.get(`board/${boardId}`);
-  return board;
+	// console.log(boardId);
+	// const board = await storageService.get('board', boardId);
+	const board = await httpService.get(`board/${boardId}`);
+	return board;
 }
 
 async function save(board) {
-  // const emptyBoard = _getEmptyBoard(board);
-  // TODO to make a function that creates an empty object
-  // return await httpService.post('board', emptyBoard);
-  console.log('hi');
+	// const emptyBoard = _getEmptyBoard(board);
+	// TODO to make a function that creates an empty object
+	// return await httpService.post('board', emptyBoard);
+	console.log('hi');
 }
 
 async function update(board) {
-  return await httpService.put(`board/${board._id}`, board);
+	return await httpService.put(`board/${board._id}`, board);
 }
 
 async function remove(boardId) {
-  // return storageService.remove('board', boardId);
-  return await httpService.delete(`board/${boardId}`);
+	// return storageService.remove('board', boardId);
+	return await httpService.delete(`board/${boardId}`);
 }
